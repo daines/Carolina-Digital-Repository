9dda328a0d640c41e22e0022475c1be24943a9b5
Gregory Jansen <count0@email.unc.edu>
2013-04-08 10:22:40 -0400

    made embargo tag depend upon a date comparison fixes #CDR-220

--- solr-search/src/main/java/edu/unc/lib/dl/search/solr/tags/AccessRestrictionsTagProvider.java
+++ solr-search/src/main/java/edu/unc/lib/dl/search/solr/tags/AccessRestrictionsTagProvider.java
@@ -1,19 +1,27 @@
 package edu.unc.lib.dl.search.solr.tags;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Set;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.unc.lib.dl.acl.util.AccessGroupSet;
-import edu.unc.lib.dl.acl.util.Permission;
 import edu.unc.lib.dl.acl.util.UserRole;
 import edu.unc.lib.dl.search.solr.model.BriefObjectMetadata;
 import edu.unc.lib.dl.search.solr.model.Tag;
 import edu.unc.lib.dl.util.ContentModelHelper;
 
 public class AccessRestrictionsTagProvider implements TagProvider {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(AccessRestrictionsTagProvider.class);
 	private static final String[] PUBLIC = new String[] { "public" };
-	private static final String EMBARGO_URI = ContentModelHelper.CDRProperty.embargoUntil.getURI().toString();
+	private static final String EMBARGO_URI = ContentModelHelper.CDRProperty.embargoUntil
+			.getURI().toString();
 
 	@Override
 	public void addTags(BriefObjectMetadata record, AccessGroupSet accessGroups) {
@@ -49,12 +57,25 @@ public class AccessRestrictionsTagProvider implements TagProvider {
 		}
 
 		// embargo
-		for(String rel : record.getRelations()) {
-			if(rel.startsWith(EMBARGO_URI)) {
-				String text = new StringBuilder("This object is embargoed until ")
-				.append(rel.substring(EMBARGO_URI.length()+1))
-				.toString();
-				record.addTag(new Tag("embargoed", text));
+		for (String rel : record.getRelations()) {
+			if (rel.startsWith(EMBARGO_URI)) {
+				try {
+					// parse the date and compare with now..
+					XMLGregorianCalendar cal = DatatypeFactory.newInstance()
+							.newXMLGregorianCalendar(
+									rel.substring(EMBARGO_URI.length() + 1).trim());
+					if (cal.toGregorianCalendar().compareTo(
+							GregorianCalendar.getInstance()) > 0) {
+						String text = new StringBuilder(
+								"This object is embargoed until ").append(
+								cal.toXMLFormat()).toString();
+						record.addTag(new Tag("embargoed", text));
+					}
+				} catch (DatatypeConfigurationException e) {
+					LOG.error(
+							"Cannot get DatatypeFactory to parse embargo XML dates.",
+							e);
+				}
 			}
 		}
 	}
